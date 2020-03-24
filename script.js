a0bd59464e1137803fba01c0d8475bda81a2f21e
kendrick-dot <k3n.jean@gmail.com>
2020-03-23 21:35:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var char = 'abcdefghijklmnopqrstuvwxyz';
+var num = '1234567890';
+var sym = '!@#$%^&*()';
+var charNum = document.getElementById("charNum");
+var numBox = document.getElementById("num");
+var symBox = document.getElementById("sym");
+var submit = document.getElementById("submit");
+var yourPw = document.getElementById("yourPw");
+
+for(var i=0; i < l i++)
+Math.floor(Math.random) 
